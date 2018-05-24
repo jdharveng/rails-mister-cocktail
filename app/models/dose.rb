737842ad2063f78657582361e12a61e87f79db4a
jerome dharveng <jerome.dharveng@hotmail.com>
2018-05-24 10:29:15 +0100

    Models and first validations

---
+++ app/models/dose.rb
@@ -0,0 +1,9 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+
+  validates :description, uniqueness: true, presence: true
+  validates :cocktail, presence: true
+  validates :ingredient, presence: true
+  validates [:cocktail, :ingredient], uniqueness: true
+end
